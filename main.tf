676873c0d7142edfa999782cf1e7044ae37f1051
Seongtae <stae1102@gmail.com>
2024-01-04 17:51:29 +0900

    feat: add ec2 count

--- main.tf
+++ main.tf
@@ -128,16 +128,19 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_eip" "hashicat" {
-  instance = aws_instance.hashicat.id
+  count = var.ec2_count
+  instance = aws_instance.hashicat[count.index].id
   vpc      = true
 }
 
 resource "aws_eip_association" "hashicat" {
-  instance_id   = aws_instance.hashicat.id
-  allocation_id = aws_eip.hashicat.id
+  count = var.ec2_count
+  instance_id   = aws_instance.hashicat[count.index].id
+  allocation_id = aws_eip.hashicat[count.index].id
 }
 
 resource "aws_instance" "hashicat" {
+  count = var.ec2_count
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
@@ -164,6 +167,7 @@ resource "aws_instance" "hashicat" {
 # Run the deploy_app.sh script.
 resource "null_resource" "configure_cat_app" {
   depends_on = [aws_eip_association.hashicat]
+  count = var.ec2_count
 
   // triggers = {
   //   build_number = timestamp()
@@ -177,7 +181,7 @@ resource "null_resource" "configure_cat_app" {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
+      host        = aws_eip.hashicat[count.index].public_ip
     }
   }
 
@@ -199,7 +203,7 @@ resource "null_resource" "configure_cat_app" {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
+      host        = aws_eip.hashicat[count.index].public_ip
     }
   }
 }
